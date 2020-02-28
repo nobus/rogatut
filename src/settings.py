c5816885772e0d9d929e389712bec4ad788531dc
nobus <meganobus@gmail.com>
2020-02-28 15:47:43 +0300

    Add more settings

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,5 @@
+import tcod as libtcodpy
+
 # ######################################################################
 # Global Game Settings
 # ######################################################################
@@ -8,3 +10,9 @@ SCREEN_HEIGHT = 50  # characters tall
 LIMIT_FPS = 20  # 20 frames-per-second maximum
 # Game Controls
 TURN_BASED = True  # turn-based game
+
+COLOR_DARK_WALL = libtcodpy.Color(0, 0, 100)
+COLOR_DARK_GROUND = libtcodpy.Color(50, 50, 150)
+
+MAP_WIDTH = 80
+MAP_HEIGHT = 45
