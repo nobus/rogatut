98a82c1f4bd9dab38283abcace32013fcd747817
nobus <meganobus@gmail.com>
2020-02-28 13:55:58 +0300

    Total refactoring

---
+++ src/player.py
@@ -0,0 +1,7 @@
+import settings
+
+
+class Player:
+    # Setup player
+    player_x = settings.SCREEN_WIDTH // 2
+    player_y = settings.SCREEN_HEIGHT // 2
---
+++ src/settings.py
@@ -0,0 +1,10 @@
+# ######################################################################
+# Global Game Settings
+# ######################################################################
+# Windows Controls
+FULLSCREEN = False
+SCREEN_WIDTH = 80  # characters wide
+SCREEN_HEIGHT = 50  # characters tall
+LIMIT_FPS = 20  # 20 frames-per-second maximum
+# Game Controls
+TURN_BASED = True  # turn-based game
