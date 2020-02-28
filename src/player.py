44f2345d60b4b2b5bd983e7a83f838d074f9fe06
nobus <meganobus@gmail.com>
2020-02-28 15:00:28 +0300

    - add abstraction CommonObject and MovingObject - add Npc and Player as implementation of that abstraction

--- src/player.py
+++ src/player.py
@@ -1,7 +1,41 @@
+import tcod as libtcodpy
+
 import settings
+from abc import ABCMeta, abstractmethod, abstractproperty
 
 
-class Player:
-    # Setup player
-    player_x = settings.SCREEN_WIDTH // 2
-    player_y = settings.SCREEN_HEIGHT // 2
+class CommonObject(metaclass=ABCMeta):
+    def __init__(self, con, x, y, char, color):
+        self.con = con
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+
+    @abstractmethod
+    def draw(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def clear(self):
+        raise NotImplementedError
+
+class MovingObject(metaclass=ABCMeta):
+    @abstractmethod
+    def move(self, dx, dy):
+        raise NotImplementedError
+
+class Npc(CommonObject, MovingObject):
+    def draw(self):
+        libtcodpy.console_set_default_foreground(self.con, self.color)
+        libtcodpy.console_put_char(self.con, self.x, self.y, self.char, libtcodpy.BKGND_NONE)
+
+    def clear(self):
+        libtcodpy.console_put_char(self.con, self.x, self.y, ' ', libtcodpy.BKGND_NONE)
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+class Player(Npc):
+    pass
